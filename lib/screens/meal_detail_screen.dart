16b524e4dd219ee6e6246dfa1dc04734ad019c79
akashVerma <akashv000@gmail.com>
2021-01-30 18:28:58 +0530

    changes!!!

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -12,10 +12,11 @@ class MealDetailScreen extends StatelessWidget {
         DUMMY_MEALS.firstWhere((element) => element.id == mealId);
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text('$mealId'),
-        ),
-        body: Column(
+      appBar: AppBar(
+        title: Text(selectedMeal.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
           children: <Widget>[
             Container(
               child: Image.network(selectedMeal.imageUrl, fit: BoxFit.cover),
@@ -23,9 +24,73 @@ class MealDetailScreen extends StatelessWidget {
               height: 300,
             ),
             Container(
-              // child: ,
+              margin: EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                'Ingredients',
+                style: Theme.of(context).textTheme.title,
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.amber)),
+              width: 300,
+              height: 200,
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.all(10),
+              child: ListView.builder(
+                itemBuilder: (ctx, index) => Card(
+                  color: Theme.of(context).accentColor,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 10,
+                    ),
+                    child: Text(selectedMeal.ingredients[index]),
+                  ),
+                ),
+                itemCount: selectedMeal.ingredients.length,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                'Steps',
+                style: Theme.of(context).textTheme.title,
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.amber)),
+              width: 300,
+              height: 200,
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.all(10),
+              child: ListView.builder(
+                itemBuilder: (ctx, index) => Card(
+                  color: Theme.of(context).accentColor,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 10,
+                    ),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        child: Text('# ${index + 1}'),
+                      ),
+                      title: Text(selectedMeal.steps[index]),
+                    ),
+                  ),
+                ),
+                itemCount: selectedMeal.steps.length,
+              ),
             )
           ],
-        ));
+        ),
+      ),
+    );
   }
 }
